00fd26fbbb5c5ca248a5a9ee3a523649146e8b80
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-08-28 00:39:50 +0700

    format code

--- predict.py
+++ predict.py
@@ -5,9 +5,7 @@ from gector.utils.helpers import read_lines, normalize
 from gector.gec_model import GecBERTModel
 
 
-def predict_for_file(
-    input_file, output_file, model, batch_size=32, to_normalize=False
-):
+def predict_for_file(input_file, output_file, model, batch_size=32, to_normalize=False):
     test_data = read_lines(input_file)
     predictions = []
     cnt_corrections = 0
@@ -34,32 +32,33 @@ def predict_for_file(
 
 
 def load_for_demo(use_roberta=True):
-    model_path = os.path.join(os.path.dirname(__file__), 'models')
+    model_path = os.path.join(os.path.dirname(__file__), "models")
     if use_roberta:
-        model_path = os.path.join(model_path, 'roberta_1_gectorv2.th')
-        transformer_model = 'roberta'
+        model_path = os.path.join(model_path, "roberta_1_gectorv2.th")
+        transformer_model = "roberta"
         special_tokens_fix = 1
         min_error_prob = 0.50
         confidence_bias = 0.20
     else:
-        model_path = os.path.join(model_path, 'xlnet_0_gector.th')
-        transformer_model = 'xlnet'
+        model_path = os.path.join(model_path, "xlnet_0_gector.th")
+        transformer_model = "xlnet"
         special_tokens_fix = 0
         min_error_prob = 0.66
         confidence_bias = 0.35
-    vocab_path = os.path.join(os.path.dirname(
-        __file__), 'data', 'output_vocabulary', '')
-    model = GecBERTModel(vocab_path=vocab_path,
-                         model_paths=[model_path],
-                         max_len=50,
-                         min_len=3,
-                         iterations=5,
-                         min_error_probability=min_error_prob,
-                         lowercase_tokens=False,
-                         model_name=transformer_model,
-                         special_tokens_fix=special_tokens_fix,
-                         log=False,
-                         confidence=confidence_bias)
+    vocab_path = os.path.join(os.path.dirname(__file__), "data", "output_vocabulary", "")
+    model = GecBERTModel(
+        vocab_path=vocab_path,
+        model_paths=[model_path],
+        max_len=50,
+        min_len=3,
+        iterations=5,
+        min_error_probability=min_error_prob,
+        lowercase_tokens=False,
+        model_name=transformer_model,
+        special_tokens_fix=special_tokens_fix,
+        log=False,
+        confidence=confidence_bias,
+    )
     return model
 
 
@@ -79,7 +78,7 @@ def predict_for_demo(lines, model, batch_size=32):
         predictions.extend(preds)
 
     # output = '<eos>'.join([' '.join(x) for x in predictions])
-    output = [' '.join(x) for x in predictions]
+    output = [" ".join(x) for x in predictions]
     return output
 
 
@@ -116,47 +115,17 @@ def main(args):
 if __name__ == "__main__":
     # read parameters
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--model_path", help="Path to the model file.", nargs="+", required=True
-    )
-    parser.add_argument(
-        "--vocab_path",
-        help="Path to the model file.",
-        default="test_fixtures/roberta_model/vocabulary",  # to use pretrained models
-    )
-    parser.add_argument(
-        "--input_file", help="Path to the evalset file", required=True
-    )
-    parser.add_argument(
-        "--output_file", help="Path to the output file", required=True
-    )
-    parser.add_argument(
-        "--max_len",
-        type=int,
-        help="The max sentence length" "(all longer will be truncated)",
-        default=50,
-    )
-    parser.add_argument(
-        "--min_len",
-        type=int,
-        help="The minimum sentence length"
-        "(all longer will be returned w/o changes)",
-        default=3,
-    )
-    parser.add_argument(
-        "--batch_size",
-        type=int,
-        help="The size of hidden unit cell.",
-        default=128,
-    )
-    parser.add_argument(
-        "--lowercase_tokens",
-        type=int,
-        help="Whether to lowercase tokens.",
-        default=0,
-    )
+    parser.add_argument("--model_path", help="Path to the model file.", default=["roberta_1_gectorv2.th"], nargs="+")
+    parser.add_argument("--vocab_path", help="Path to the model file.", default="test_fixtures/roberta_model/vocabulary")
+    parser.add_argument("--input_file", help="Path to the evalset file", required=True)
+    parser.add_argument("--output_file", help="Path to the output file", required=True)
+    parser.add_argument("--max_len", help="The max sentence length" "(all longer will be truncated)", default=50, type=int)
+    parser.add_argument("--min_len", help="The minimum sentence length" "(all longer will be returned w/o changes)", default=3, type=int)
+    parser.add_argument("--batch_size", help="The size of hidden unit cell.", default=128, type=int)
+    parser.add_argument("--lowercase_tokens", help="Whether to lowercase tokens.", default=0, type=int)
     parser.add_argument(
         "--transformer_model",
+        help="Name of the transformer model.",
         choices=[
             "bert",
             "gpt2",
@@ -168,52 +137,26 @@ if __name__ == "__main__":
             "roberta-large",
             "xlnet-large",
         ],
-        help="Name of the transformer model.",
         default="roberta",
     )
-    parser.add_argument(
-        "--iteration_count",
-        type=int,
-        help="The number of iterations of the model.",
-        default=5,
-    )
-    parser.add_argument(
-        "--additional_confidence",
-        type=float,
-        help="How many probability to add to $KEEP token.",
-        default=0,
-    )
-    parser.add_argument(
-        "--additional_del_confidence",
-        type=float,
-        help="How many probability to add to $DELETE token.",
-        default=0,
-    )
-    parser.add_argument(
-        "--min_error_probability",
-        type=float,
-        help="Minimum probability for each action to apply. "
-        "Also, minimum error probability, as described in the paper.",
-        default=0.0,
-    )
+    parser.add_argument("--iteration_count", help="The number of iterations of the model.", default=5, type=int)
+    parser.add_argument("--additional_confidence", help="How many probability to add to $KEEP token.", default=0, type=float)
+    parser.add_argument("--additional_del_confidence", help="How many probability to add to $DELETE token.", default=0, type=float)
+    parser.add_argument("--min_error_probability", help="Minimum probability for each action to apply, minimum error probability", default=0, type=float)
     parser.add_argument(
         "--special_tokens_fix",
-        type=int,
         help="Whether to fix problem with [CLS], [SEP] tokens tokenization. "
         "For reproducing reported results it should be 0 for BERT/XLNet and 1 for RoBERTa.",
         default=1,
+        type=int,
     )
-    parser.add_argument(
-        "--is_ensemble", type=int, help="Whether to do ensembling.", default=0
-    )
-    parser.add_argument(
-        "--weights",
-        help="Used to calculate weighted average",
-        nargs="+",
-        default=None,
-    )
-    parser.add_argument(
-        "--normalize", help="Use for text simplification.", action="store_true"
-    )
+    parser.add_argument("--is_ensemble", type=int, help="Whether to do ensembling.", default=0)
+    parser.add_argument("--weights", help="Used to calculate weighted average", default=None, nargs="+")
+    parser.add_argument("--normalize", help="Use for text simplification.", action="store_true")
+
     args = parser.parse_args()
+
+    model_folder = os.path.join(os.path.dirname(__file__), "models")
+    args.model_path = [os.path.join(model_folder, path) for path in args.model_path]
+
     main(args)
