1737617134d5379afd1e97514e716eae9c6fb377
Ngô Văn Cảnh <canh25xp@gmail.com>
2025-02-08 13:57:05 +0700

    predict.py: add predict and load fucntion

--- predict.py
+++ predict.py
@@ -1,13 +1,53 @@
 import argparse
+import os
+import sys
+from pathlib import Path
+
+FILE = Path(__file__).resolve()
+ROOT = FILE.parents[0]  # Gector root directory
+if str(ROOT) not in sys.path:
+    sys.path.append(str(ROOT))  # add ROOT to PATH
+ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
 
 from gector.utils.helpers import read_lines, normalize
 from gector.gec_model import GecBERTModel
+from transformers import logging
+
+logging.set_verbosity_error()
 
 
-def predict_for_file(
-    input_file, output_file, model, batch_size=32, to_normalize=False
-):
-    test_data = read_lines(input_file)
+def load(model_path, transformer_model):
+    if transformer_model == "roberta":
+        special_tokens_fix = 1
+        min_error_prob = 0.50
+        confidence_bias = 0.20
+    elif transformer_model == "xlnet":
+        special_tokens_fix = 0
+        min_error_prob = 0.50
+        confidence_bias = 0.20
+    elif transformer_model == "bert":
+        special_tokens_fix = 0
+        min_error_prob = 0.41
+        confidence_bias = 0.10
+
+    model = GecBERTModel(
+        vocab_path=ROOT / "test_fixtures/roberta_model/vocabulary",
+        model_paths=[model_path],
+        max_len=50,
+        min_len=3,
+        iterations=5,
+        min_error_probability=min_error_prob,
+        lowercase_tokens=False,
+        model_name=transformer_model,
+        special_tokens_fix=special_tokens_fix,
+        log=False,
+        confidence=confidence_bias,
+    )
+    return model
+
+
+def predict(lines, model, batch_size=32):
+    test_data = [s.strip() for s in lines]  # Remove trailling spaces
     predictions = []
     cnt_corrections = 0
     batch = []
@@ -24,11 +64,22 @@ def predict_for_file(
         cnt_corrections += cnt
 
     result_lines = [" ".join(x) for x in predictions]
+    return result_lines, cnt_corrections
+
+
+def predict_for_file(input_file, output_file, model, batch_size=32, to_normalize=False):
+    test_data = read_lines(input_file)
+    result_lines, cnt_corrections = predict(test_data, model, batch_size)
+
     if to_normalize:
         result_lines = [normalize(line) for line in result_lines]
 
-    with open(output_file, "w") as f:
-        f.write("\n".join(result_lines) + "\n")
+    if output_file:
+        with open(output_file, "w") as f:
+            f.write("\n".join(result_lines) + "\n")
+    else:
+        print("\n".join(result_lines))
+
     return cnt_corrections
 
 
@@ -71,13 +122,13 @@ if __name__ == "__main__":
     parser.add_argument(
         "--vocab_path",
         help="Path to the model file.",
-        default="data/output_vocabulary",  # to use pretrained models
+        default=ROOT/"test_fixtures/roberta_model/vocabulary",  # to use pretrained models
     )
     parser.add_argument(
         "--input_file", help="Path to the evalset file", required=True
     )
     parser.add_argument(
-        "--output_file", help="Path to the output file", required=True
+        "--output_file", help="Path to the output file", default=None
     )
     parser.add_argument(
         "--max_len",
